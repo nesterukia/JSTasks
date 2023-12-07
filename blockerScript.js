2a80ebcf18b5a4cdc8b3fbb0319f3a9af711d432
nesterukia <77958906+nesterukia@users.noreply.github.com>
2023-12-07 19:25:55 +0300

    Update blockerScript.js

--- blockerScript.js
+++ blockerScript.js
@@ -1,7 +1,19 @@
+// ----------Куча комментариев----------------------------------------------
+//
+// ==UserScript==
+// @name Hello World
+// @namespace absolvo.ru
+// @version 0.01
+// @source absolvo.ru
+// @description Этот скрипт покажет вам алерт с "hello word" на каждой странице, кроме исключений!
+// @include *
+// @exclude http://*.habrahabr.ru/*
+// @exclude habrahabr.ru*
+// @exclude absolvo.ru*
+// ==/UserScript==
 const arr = ["shorts", "instagram"];
-
 arr.forEach((s)=>{
   if(window.location.href.includes(s)){
-     window.location.replace("https://www.coursera.org/professional-certificates/meta-front-end-developer");
+    window.location.replace("https://www.coursera.org/professional-certificates/meta-front-end-developer");
   }
 });
